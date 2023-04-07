97e4800671d9068c35c6a66342a7b918995d3dfe
manu-saini94 <manu.saini94@gmail.com>
2023-04-07 20:59:02 +0530

    added: calculate daily wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,14 +1,27 @@
 package com.bridgelabz;
 
+// Naming conventions
+// variable -> camel case -> isPresent
+// method -> camel case -> calculateWage()
+// class and interface -> Pascal case -> EmployeeWage
+// Constants -> All uppercase with underscores
+// package -> all lowercase
 public class EmployeeWage {
+
+    static final int WAGE_PER_HOUR = 20;
+
+    static final int FULL_DAY_HOUR = 8;
 
     public static void main(String[] args) {
         System.out.println("Welcome to employeewage");
         int isPresent = (int) (Math.random() * 10) % 2;
-        if (isPresent == 1)
+        int dailyWage = 0;
+        if (isPresent == 1) {
+            dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println("Employee is Present!");
-        else
+        } else {
             System.out.println("Employee is Absent!");
-
+        }
+        System.out.println("Daily wage => " + dailyWage);
     }
 }
